b29b3bea9e2363074b46bf4586f54728eaab3761
namesty <nestor09amesty@gmail.com>
2021-07-12 01:10:26 +0200

    Initial setup

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { Client } from "discord.js"
+import { config } from "dotenv"
+
+config()
+
+const client = new Client()
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+  })
+  
+  client.on("message", msg => {
+    if (msg.content === "ping") {
+      msg.reply("pong");
+    }
+  })
+
+client.login(process.env.TOKEN)
