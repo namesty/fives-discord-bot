4deb0b4ec6bbaa6613e68b77d05451be4a937d81
namesty <nestor09amesty@gmail.com>
2021-07-12 01:24:19 +0200

    addToList

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
-import { Client } from "discord.js"
+import { Client, Message } from "discord.js"
 import { config } from "dotenv"
 
 config()
 
 const client = new Client()
 
+const commands = ["add", "remove"]
+let currentList: string[] = []
+
+const addPlayerToList = (message: Message) => {
+    const newMembers = message.content.split("$add")[1].split(",").map(memberString => memberString.trim())
+    currentList = [...currentList, ...newMembers]
+
+    message.reply(`
+        Current List: ${currentList.join(`\n-`)}
+    `)
+}
+
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
   })
   
   client.on("message", msg => {
-    if (msg.content === "ping") {
-      msg.reply("pong");
+    if(msg.content.startsWith("$add")) {
+        addPlayerToList(msg)
     }
   })
 
